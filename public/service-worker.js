09479e59b1e4bd241b94e1a4eb5766f70e2851fe
Alice Piar <adpir30@gmail.com>
2021-02-21 14:08:11 -0600

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,29 +4,34 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/favicon.ico",
   "/manifest.webmanifest",
   "/style.css",
   "/db.js",
   "/icons/icon-192x192.png",
-  "/icons/icon-512-512.png",
+  "/icons/icon-512x512.png",
  
 ];
 
 // install
+// self.addEventListener("install", function (evt) {
+//   // pre cache all static assets
+//   evt.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+//   );
+
+//   // tell the browser to activate this service worker immediately once it
+//   // has finished installing
+//   self.skipWaiting();
+
+// });
 self.addEventListener("install", function (evt) {
-  // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
-    
-  // pre cache all static assets
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
 
